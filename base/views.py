d52642efd62245bd70dd2ddc38be0dc26e5b0e8e
Satyam9993 <satyamkatiyar9993@gmail.com>
2023-03-10 15:04:47 +0530

    user profile

--- base/views.py
+++ base/views.py
@@ -70,7 +70,9 @@ def home(request):
         )
     topics = Topic.objects.all()
     room_count = rooms.count()
-    context = {'rooms': rooms, 'topics' : topics, 'room_count':room_count}
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
+
+    context = {'rooms': rooms, 'topics' : topics, 'room_count':room_count, 'room_messages':room_messages}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
@@ -88,6 +90,14 @@ def room(request, pk):
 
     context = {'room' : room, 'messages':messages, 'participants':participants}
     return render(request, 'base/room.html', context)
+
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {'user':user, 'rooms':rooms,'topics':topics, 'room_messages' : room_messages}
+    return render(request, 'base/profile.html', context)
 
 @login_required(login_url = 'login')
 def createRoom(request):
@@ -127,7 +137,6 @@ def deleteRoom(request, pk):
         return redirect('home')
     context={'obj': room}
     return render(request, 'base/delete.html', context)
-
 
 @login_required(login_url = 'login')
 def deleteMessage(request, pk):
